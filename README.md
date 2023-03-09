55c7ca90e23c6a480998248304de677377119d8a
Ivan <ivanvolodin960@gmail.com>
2023-03-09 19:17:47 +0300

    feat: add file-loader in webpack config

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ To install packages, use the command
 npm install
 ```
 
+---
+
+\* Before start project you need to make file `.env` in root of project and copy from file `.env.example` variables with meanings in new file. Or you can write your own meanings for variables in file `.env`
+
 ## Commands
 
 ### Launching a development server
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
-const webpack = require('webpack');
-const dotenv = require('dotenv');
+const path = require("path");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
 
 /* Plugins */
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 // const CopyPlugin = require('copy-webpack-plugin');
 
 /* Variables env */
 const env = dotenv.config().parsed;
 const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next])
-  return prev
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
 }, {});
 
 /* Variables for dev and prod */
-const mode = process.env.NODE_ENV || 'development';
-const devMode = mode === 'development';
-const target = devMode ? 'web' : 'browserslist';
-const devtool = devMode ? 'source-map' : undefined;
+const mode = process.env.NODE_ENV || "development";
+const devMode = mode === "development";
+const target = devMode ? "web" : "browserslist";
+const devtool = devMode ? "source-map" : undefined;
 
 /* Config */
 module.exports = {
@@ -28,32 +28,32 @@ module.exports = {
   devtool,
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "dist")
     },
     port: process.env.PORT || 3000,
     open: true,
-    hot: true,
+    hot: true
   },
   stats: {
     children: false,
-    modulesSpace: 0,
+    modulesSpace: 0
   },
-  entry: ["@babel/polyfill", path.resolve(__dirname, 'src', 'index.js')],
+  entry: ["@babel/polyfill", path.resolve(__dirname, "src", "index.js")],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     clean: true,
-    filename: '[name].[contenthash].js',
-    assetModuleFilename: 'assets/[name][ext]',
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "assets/[name][ext]"
   },
   plugins: [
     new webpack.DefinePlugin(envKeys),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
+      template: path.resolve(__dirname, "src", "index.html")
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
+      filename: "[name].[contenthash].css"
     }),
-    new ESLintPlugin({ fix: true }),
+    new ESLintPlugin({ fix: true })
     // new CopyPlugin({
     //   patterns: [{ from: 'static', to: './' }],
     // }),
@@ -62,39 +62,50 @@ module.exports = {
     rules: [
       {
         test: /\.html$/i,
-        loader: 'html-loader',
+        loader: "html-loader"
       },
       {
         test: /\.(c|sa|sc)ss$/i,
         use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [require('postcss-preset-env')],
-              },
-            },
+                plugins: [require("postcss-preset-env")]
+              }
+            }
           },
-          'group-css-media-queries-loader',
+          "group-css-media-queries-loader",
           {
-            loader: 'resolve-url-loader',
+            loader: "resolve-url-loader"
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
-              sourceMap: true,
-            },
-          },
-        ],
+              sourceMap: true
+            }
+          }
+        ]
       },
       {
         test: /\.woff2?$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'fonts/[name][ext]',
-        },
+          filename: "fonts/[name][ext]"
+        }
+      },
+      {
+        test: /\.(wav)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets"
+            }
+          }
+        ]
       },
       {
         test: /\.(jpe?g|png|webp|gif|svg)$/i,
@@ -102,39 +113,39 @@ module.exports = {
           ? []
           : [
               {
-                loader: 'image-webpack-loader',
+                loader: "image-webpack-loader",
                 options: {
                   mozjpeg: {
-                    progressive: true,
+                    progressive: true
                   },
                   optipng: {
-                    enabled: false,
+                    enabled: false
                   },
                   pngquant: {
                     quality: [0.65, 0.9],
-                    speed: 4,
+                    speed: 4
                   },
                   gifsicle: {
-                    interlaced: false,
+                    interlaced: false
                   },
                   webp: {
-                    quality: 75,
-                  },
-                },
-              },
+                    quality: 75
+                  }
+                }
+              }
             ],
-        type: 'asset/resource',
+        type: "asset/resource"
       },
       {
         test: /\.m?js$/i,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
-  },
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
 };
